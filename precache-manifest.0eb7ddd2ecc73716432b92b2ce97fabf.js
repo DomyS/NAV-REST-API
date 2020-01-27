ac19db0d08fabba3222c8ae426681a18749d17bb
Domante Stirbyte <domante.stirbyte@gmail.com>
2020-01-27 15:26:40 +0100

    Updates

---
+++ precache-manifest.0eb7ddd2ecc73716432b92b2ce97fabf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ec9c21724065d6e815f60bb5f5df5f",
+    "url": "/nav-rest-api/index.html"
+  },
+  {
+    "revision": "3a9546cec9ee8a250e33",
+    "url": "/nav-rest-api/static/css/main.2a83c7c0.chunk.css"
+  },
+  {
+    "revision": "42cbd9e680619a0453eb",
+    "url": "/nav-rest-api/static/js/2.0c697dce.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/nav-rest-api/static/js/2.0c697dce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a9546cec9ee8a250e33",
+    "url": "/nav-rest-api/static/js/main.aa10e1c9.chunk.js"
+  },
+  {
+    "revision": "c9123870b0a5c533e7b1",
+    "url": "/nav-rest-api/static/js/runtime-main.1b129cd7.js"
+  }
+]);
